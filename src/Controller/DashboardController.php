80f2d36b810d27e32c55701f5ca00a97a433347c
Dominique MICHEL <dominique.michel@epita.fr>
2020-12-08 02:11:37 +0100

    ADD default controller + inertia template page generator

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Rompetomp\InertiaBundle\Service\InertiaInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractController
+{
+    /**
+     * @Route("/dashboard", name="dashboard")
+     */
+    public function index(InertiaInterface $inertia): Response
+    {
+        return $inertia->render('Dashboard', [
+            'prop' => 'title',
+        ]);
+    }
+}
